672c9899fb567013e42adad2013f556118d7672e
sammyb102 <samcori@udel.edu>
2022-02-14 12:04:52 -0500

    forgot to commit the rest but its done now

--- src/functions.ts
+++ src/functions.ts
@@ -12,7 +12,18 @@ export function fahrenheitToCelius(temperature: number): number {
  * if the number is greater than zero.
  */
 export function add3(first: number, second: number, third: number): number {
-    return 0;
+    // eslint-disable-next-line @typescript-eslint/no-inferrable-types
+    let total: number = 0;
+    if (first >= 0) {
+        total += first;
+    }
+    if (second >= 0) {
+        total += second;
+    }
+    if (third >= 0) {
+        total += third;
+    }
+    return total;
 }
 
 /**
@@ -20,7 +31,7 @@ export function add3(first: number, second: number, third: number): number {
  * mark added to the end.
  */
 export function shout(message: string): string {
-    return "";
+    return message.toUpperCase() + "!";
 }
 
 /**
@@ -28,7 +39,7 @@ export function shout(message: string): string {
  * mark. Do not use an `if` statement in solving this question.
  */
 export function isQuestion(message: string): boolean {
-    return true;
+    return message.charAt(message.length - 1) === "?";
 }
 
 /**
@@ -37,5 +48,11 @@ export function isQuestion(message: string): boolean {
  * upper or lower case), then return `false`. Otherwise, return `null`.
  */
 export function convertYesNo(word: string): boolean | null {
-    return true;
+    word = word.toLowerCase();
+    if (word === "yes") {
+        return true;
+    } else if (word === "no") {
+        return false;
+    }
+    return null;
 }
