5fab2cc49b76ae803131f666605799d1fae62dc3
sammyb102 <samcori@udel.edu>
2022-03-01 15:05:07 -0500

    reveal answer

--- src/App.tsx
+++ src/App.tsx
@@ -14,41 +14,6 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-<<<<<<< HEAD
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
-            <p>
-                samuel bernal<br></br>Hello World
-            </p>
-            <div>
-                <h1>This is the start of task 3 stuff</h1>
-                <img
-                    src="https://i2.wp.com/www.australiangeographic.com.au/wp-content/uploads/2021/08/echidna-train.jpg?resize=2048%2C1170&ssl=1"
-                    alt="This is a picture of an echidna train"
-                    style={{ width: "30%" }}
-                />
-            </div>
-            <Container>
-                <Row>
-                    <Col>
-                        <div className="redRect"></div>
-                        <ul>
-                            <li>One Item</li>
-                            <li>Another Item</li>
-                            <li>And a third</li>
-                        </ul>
-                    </Col>
-                    <Col>
-                        <div className="redRect"></div>
-                        <Button onClick={() => console.log("Hello World!")}>
-                            Log Hello World
-                        </Button>
-                    </Col>
-                </Row>
-            </Container>
-=======
             <hr></hr>
             <Counter></Counter>
             <hr />
@@ -61,7 +26,6 @@ function App(): JSX.Element {
             <ChangeType></ChangeType>
             <hr />
             <CycleHoliday></CycleHoliday>
->>>>>>> upstream/task-state
         </div>
     );
 }
--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,16 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [visible, showAnswer] = useState<boolean>(false);
+
+    function showHide(): void {
+        showAnswer(!visible);
+    }
+
+    return (
+        <div>
+            <Button onClick={showHide}>Show Answer</Button>
+            {visible && <p>42</p>}
+        </div>
+    );
 }
