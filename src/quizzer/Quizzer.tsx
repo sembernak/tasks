5b02d3d785cdad66a2f25a3f4052e587cb2547ab
sammyb102 <samcori@udel.edu>
2022-03-14 11:47:09 -0400

    minimally viable

--- src/quizzer/Quizzer.tsx
+++ src/quizzer/Quizzer.tsx
@@ -1,5 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
+import { Quiz } from "./Quiz";
+
+const INTITALQUIZ = [
+    <Quiz key="1"></Quiz>,
+    <Quiz key="2"></Quiz>,
+    <Quiz key="3"></Quiz>
+];
 
 export function Quizzer(): JSX.Element {
-    return <h3>Quizzer</h3>;
+    const [quizList, editList] = useState<JSX.Element[]>(INTITALQUIZ);
+
+    return (
+        <div>
+            {quizList}
+            <Button
+                onClick={() => editList([...quizList, <Quiz key="4"></Quiz>])}
+            >
+                Add Quiz
+            </Button>
+        </div>
+    );
 }
